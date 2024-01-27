a6cc929eda2abc8e34715a36fc182439cfb0ee68
FabioSebs <sebsphotosofficial@gmail.com>
2024-01-27 23:53:50 +0700

    hotels from ta

--- main.py
+++ main.py
@@ -1,10 +1,10 @@
-from websites import bookings
+from websites import tripadvisor
 import os
 from dotenv import load_dotenv
 load_dotenv()
 
-Bookings = bookings.BookingsScraper(
-    "https://theicct.org/insight-analysis/publications/",
-    "books.json"
+Bookings = tripadvisor.TripAdvisorScraper(
+    "https://www.tripadvisor.com/Search?searchSessionId=001291094b07f937.ssid&searchNearby=false&ssrc=h&q=bali&sid=B3D1899E7AD44DFA8EB486C24C019F291705646765454&blockRedirect=true&geo=1&rf=6",
+    "triphotels.json"
 )
 Bookings.run()
