521a38410e0c7de5cbb786b9d079863c0ecf1b9a
FabioSebs <sebsphotosofficial@gmail.com>
2024-04-18 20:16:13 +0700

    starting otto

--- main.py
+++ main.py
@@ -1,10 +1,10 @@
-from websites import evs
+from websites import tripadvisor
 import os
 from dotenv import load_dotenv
 load_dotenv()
 
-EVS = evs.EVScraper(
-    "https://www.caranddriver.com/news/g29994375/future-electric-cars-trucks/",
-    "upcoming-evs.json"
+Bookings = tripadvisor.TripAdvisorScraper(
+    "https://www.tripadvisor.com/Search?searchSessionId=001291094b07f937.ssid&searchNearby=false&ssrc=h&q=bali&sid=B3D1899E7AD44DFA8EB486C24C019F291705646765454&blockRedirect=true&geo=1&rf=6",
+    "triphotels.json"
 )
-EVS.run()
+Bookings.run()
